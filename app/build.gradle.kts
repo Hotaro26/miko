2388ac0a77c1ef019c7bcf9d66c6d24cf84e56fb
Hotaro <hotaro@example.com>
2026-07-13 17:14:15 +0530

    chore: bump version to 3.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.miko.reader"
         minSdk = 24
         targetSdk = 34
-        versionCode = 4
-        versionName = "3.1"
+        versionCode = 5
+        versionName = "3.2"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables { useSupportLibrary = true }
     }
